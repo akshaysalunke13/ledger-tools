422b92d41753974574b10db1f81c5879e565122d
John Mee <git.commit@johnmee.com>
2021-02-04 20:40:31 +1100

    Find unknowns and convert for stgeorge

--- stgeorge_test.py
+++ stgeorge_test.py
@@ -1,8 +1,13 @@
+import sys
 import unittest
 
+import accounts as acc
 import stgeorge
 
-CSV_TESTFILE = '/home/john/Downloads/trans020720.csv'
+CSV_TESTFILE = '/home/john/projects/money/john-fiona/raw/stgeorge/trans300121.csv'
+YAML_TESTFILE = '../john-fiona/accounts.yaml'
+
+BANK_ACCOUNT = 'Assets:Bank:Joint-CompleteFreedom'
 
 
 class TestStGeorgeFile(unittest.TestCase):
@@ -23,3 +28,33 @@ class TestStGeorgeFile(unittest.TestCase):
 
         for vendor in sorted(vendors):
             print("%s" % vendor)
+
+
+class TestMerchantsMerge(unittest.TestCase):
+    """Combine the two files; the transactions and the list of accounts."""
+
+    def test_find_unknown_merchants(self):
+        """print out of merchants not matched to accounts"""
+        accounts = acc.AccountFile(YAML_TESTFILE)
+        with stgeorge.StGeorgeFile(CSV_TESTFILE) as transactions:
+            # collect identical unknowns together
+            unknowns = {}
+            for record in sorted(transactions, key=lambda x: x[1]):
+                merchant = record[1]
+                if not accounts.match(merchant):
+                    if merchant not in unknowns:
+                        unknowns[merchant] = []
+                    unknowns[merchant].append(record)
+
+            # print them in order of most to least frequently occuring
+            for records in sorted(unknowns.values(), key=lambda x: len(x), reverse=True):
+                for record in records:
+                    print(record)
+
+    def test_convert(self):
+        """Convert the transactions and accounts into beancount."""
+        fh = open("generated.beancount", "w")
+        for record in stgeorge.to_beancount(CSV_TESTFILE, YAML_TESTFILE, BANK_ACCOUNT):
+            sys.stdout.write(record)
+            fh.write(record)
+        fh.close()
