52301dac6daeb5a43cfa935b8a719f5285b6317f
John Mee <git.commit@johnmee.com>
2022-05-02 21:12:34 +1000

    Brew fiona by cli for upbank

--- ledgertools/upbank.py
+++ ledgertools/upbank.py
@@ -11,7 +11,10 @@ import datetime
 import click
 import json
 import os
+import pprint
 import requests
+
+from . import accounts
 
 
 URL = "https://api.up.com.au/api/v1"
@@ -142,6 +145,14 @@ def ping():
 
 
 @cli.command()
+def categories():
+    """Get a list of transaction categories."""
+    client = UpbankClient(UPBANK_TOKEN)
+    response = client.categories()
+    click.echo(pprint.pformat(response))
+
+
+@cli.command()
 def balance():
     """Fetch the current balance of the account."""
     client = UpbankClient(UPBANK_TOKEN)
@@ -161,10 +172,41 @@ def month(year, month):
     click.echo(json.dumps(transactions, indent=3))
 
 
+@cli.command()
+@click.argument("rawfile", type=click.Path(exists=True))
+@click.argument("balance", type=click.types.FLOAT)
+def brewfiona(rawfile, balance):
+    """Generate beans from fionas upbank transaction file.
+    """
+    transactions = json.load(open(rawfile))
+    change = 0.0
+    date_str = 'No date'
+    tags = '#fiona'
+    accountname = "Assets:Bank:Fiona-Upbank"
 
-# TODO!
-# check merchants
-# generate beancount
+    for trans in reversed(transactions):
+        date_str = trans['attributes']['createdAt'][:10]
+        raw_text = trans['attributes']['rawText']
+        description = trans['attributes']['description']
+        value = - float(trans['attributes']['amount']['value'])
+        try:
+            category = ':'.join([
+                trans['relationships']['category']['data']['id'],
+                trans['relationships']['parentCategory']['data']['id'],
+            ])
+        except TypeError:
+            category = ''
+        txt_description = f"\"{raw_text} [{description}]\""
+        entry = f'{date_str} * {txt_description:66s}  {tags}\n'
+        entry += f"    {accountname}\n"
+        entry += "    %-46s %10.2f AUD\n" % (
+            f'ACCOUNT_UNKNOWN [{category}]', float(value))
+
+        click.echo(entry)
+        change += float(value)
+    balance = balance - change
+    balance_str = f"{date_str} balance {accountname:36s} {balance:.2f} AUD"
+    click.echo(balance_str)
 
 
 if __name__ == "__main__":
